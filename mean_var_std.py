9ae817c108885e9c2d1081be3d788316237f8374
Rocco Pio Mastropietro <ing.mstrcp@icloud.com>
2024-03-09 22:30:37 +0000

    update

--- mean_var_std.py
+++ mean_var_std.py
@@ -80,6 +80,3 @@ def calculate(list):    # Definisce la funzione.
         calculations['sum'] = sum_list    # Inserisce la lista dei valori di somma nel dizionario.
         
         return calculations    # Restituisce il dizionario dei calcoli.
-
-input1 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-calculate(input1)
