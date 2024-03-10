86ba81497114de957f49a79d16d2402b29332edc
Rocco Pio Mastropietro <ing.mstrcp@icloud.com>
2024-03-10 10:34:42 +0000

    list_initialization

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,11 +6,6 @@
 #
 import numpy as np    # Importa la libreria Numpy.
 
-def jls_extract_def():
-    # Restituisce il dizionario dei calcoli.
-    return 
-
-
 def calculate(list):    # Definisce la funzione.
 
     # Se nella funzione viene passato un elenco contenente meno di 9 elementi, solleva un'eccezione
@@ -22,14 +17,15 @@ def calculate(list):    # Definisce la funzione.
         array = np.array(list)    # Converte il parametro list in un array di Numpy.
         array3x3 = np.reshape(array, (3, 3))    # Trasforma l'array di dimensione 9 in un array 3x3.
         calculations = {}
-        mean_list = var_list = std_list = max_list = min_list = sum_list = []
-        mean_axis1 = var_axis1 = std_axis1 = max_axis1 = min_axis1 = sum_axis1 = []
-        mean_axis2 = var_axis2 = std_axis2 = max_axis2 = min_axis2 = sum_axis2 = []
+        empty_list = ([], [], [], [], [], [])
+        mean_list, var_list, std_list, max_list, min_list, sum_list = empty_list
+        mean_axis1, var_axis1, std_axis1, max_axis1, min_axis1, sum_axis1 = empty_list
+        mean_axis2, var_axis2, std_axis2, max_axis2, min_axis2, sum_axis2 = empty_list
 
         # I valori nel dizionario restituito sono liste e non array Numpy.
         #
         mean_axis1 = np.mean(array3x3, axis=0).tolist()    # Calcola la media lungo gli assi verticali.
-        var_axis1 = np.var(array3x3, axis=0, dtype=np.float64).tolist()    # Calcola la varianza lungo gli assi verticali.
+        var_axis1 = np.var(array3x3, axis=0).tolist()    # Calcola la varianza lungo gli assi verticali.
         std_axis1 = np.std(array3x3, axis=0).tolist()    # Calcola la dev. standard lungo gli assi verticali.
         max_axis1 = np.max(array3x3, axis=0).tolist()    # Calcola il massimo lungo gli assi verticali.
         min_axis1 = np.min(array3x3, axis=0).tolist()    # Calcola la minimo lungo gli assi verticali.
@@ -38,7 +34,7 @@ def calculate(list):    # Definisce la funzione.
         # I valori nel dizionario restituito sono liste e non array Numpy.
         #
         mean_axis2 = np.mean(array3x3, axis=1).tolist()    # Calcola la media lungo gli assi orizzontali.
-        var_axis2 = np.var(array3x3, axis=1, dtype=np.float64).tolist()    # Calcola la varianza lungo gli assi orizzontali.
+        var_axis2 = np.var(array3x3, axis=1).tolist()    # Calcola la varianza lungo gli assi orizzontali.
         std_axis2 = np.std(array3x3, axis=1).tolist()    # Calcola la dev. standard lungo gli assi orizzontali.
         max_axis2 = np.max(array3x3, axis=1).tolist()    # Calcola il massimo lungo gli assi orizzontali.
         min_axis2 = np.min(array3x3, axis=1).tolist()    # Calcola la minimo lungo gli assi orizzontali.
@@ -47,7 +43,7 @@ def calculate(list):    # Definisce la funzione.
         # I valori nel dizionario restituito sono liste e non array Numpy.
         #
         mean_flattened = np.mean(array).tolist()
-        var_flattened = np.var(array, dtype=np.float64).tolist()
+        var_flattened = np.var(array).tolist()
         std_flattened = np.std(array).tolist()
         max_flattened = np.max(array).tolist()
         min_flattened = np.min(array).tolist()
@@ -84,4 +80,4 @@ def calculate(list):    # Definisce la funzione.
         calculations['min'] = min_list    # Inserisce la lista dei valori di minimo nel dizionario.
         calculations['sum'] = sum_list    # Inserisce la lista dei valori di somma nel dizionario.
         
-        return calculations    # Restituisce il dizionario dei calcoli. = jls_extract_def()
+        return calculations    # Restituisce il dizionario dei calcoli.
